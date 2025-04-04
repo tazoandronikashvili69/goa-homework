fe3f3c6a6ad766a2bca57a80ea0760521dead991
tazoandronikashvili69 <tazukaandronikashvili69@gmail.com>
2025-04-04 23:13:24 +0400

    tazo

---
+++ day 00/homework/tazo22.py
@@ -0,0 +1,8 @@
+name = "tazo"
+surname = "andronikashvili"
+age = 12
+height = 169.5
+clas = 7
+school = 157
+
+print(name + " " + str (surname) + " " + str (age) + " " + str (height) + " " + str (clas) + " " + str (school))
