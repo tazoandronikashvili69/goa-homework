4f2340452a69b33a551cbe72d8c10b842f453955
tazoandronikashvili69 <tazukaandronikashvili69@gmail.com>
2025-05-18 21:10:58 +0400

    commit

--- day 00/homework/tazo22.py
+++
@@ -1,8 +0,0 @@
-name = "tazo"
-surname = "andronikashvili"
-age = 12
-height = 169.5
-clas = 7
-school = 157
-
-print(name + " " + str (surname) + " " + str (age) + " " + str (height) + " " + str (clas) + " " + str (school))
